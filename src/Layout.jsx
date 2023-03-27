aa53b8d333c6640fc583e9f4935327ad2a8136f5
Namo <pongsathon139@gmail.com>
2023-03-27 10:29:34 +0700

    make header fixed in large screen

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -4,10 +4,7 @@ import Header from "./components/Header";
 export default function Layout() {
   return (
     <div className="flex flex-col relative min-h-screen" id="toppage">
-      <div className="z-20">
-        <Header />
-      </div>
-
+      <Header />
       <Outlet />
     </div>
   );
