db0cd54e19cbefd18bd4a984f414883185911747
Namo <pongsathon139@gmail.com>
2023-03-31 08:33:58 +0700

    fix img in place page

--- src/pages/PlacePage.jsx
+++ src/pages/PlacePage.jsx
@@ -45,27 +45,27 @@ export default function PlacePage() {
         onClick={handleShowPhotos}
       >
         {place.photos?.[0] && (
-          <div className="flex photos-height">
+          <div className="grid photos-height">
             <PlaceImg place={place} index={0} />
           </div>
         )}
 
-        <div className="grid grid-rows-[2fr_1fr] gap-1 sm:gap-2 photos-height">
+        <div className="grid grid-rows-[2fr_1fr] gap-1 overflow-hidden sm:gap-2 photos-height">
           {place.photos?.[1] && (
-            <div className="flex overflow-hidden">
+            <div className="grid overflow-hidden">
               <PlaceImg place={place} index={1} />
             </div>
           )}
 
-          <div className="grid grid-cols-2 gap-1 sm:gap-2 relative sm:static">
+          <div className="grid grid-cols-2 gap-1 overflow-hidden sm:gap-2 relative sm:static">
             {place.photos?.[2] && (
-              <div className="flex overflow-hidden">
+              <div className="grid overflow-hidden">
                 <PlaceImg place={place} index={2} />
               </div>
             )}
 
             {place.photos?.[3] && (
-              <div className="flex overflow-hidden relative">
+              <div className="grid overflow-hidden relative">
                 <PlaceImg place={place} index={3} />
                 <button
                   onClick={handleShowPhotos}
